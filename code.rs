0756efa80381142798efe85ab23de13f7c20e5ae
Eli <121198893+tired-tux@users.noreply.github.com>
2023-07-12 16:28:13 -0500

    Update code.rs

--- code.rs
+++ code.rs
@@ -4,7 +4,11 @@ fn main() {
     for w in 1..254 {
       for e in 1..254 {
         for r in 1..254 {
-          println!("ping -c 1 -i 0.2 {q}.{w}.{e}.{r} > ips/{q}.{w}.{e}.{r}.result");
+          println!("ping -c 1 -i 0.2 {q}.{w}.{e}.{r}");
+          println!("if [ $? -eq 0 ]");
+          println!("then");
+          println!("touch ips/{q}.{w}.{e}.{r}");
+          println!("fi");
         }
       }
     }
