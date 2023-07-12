076c201cac4e6baaf1e1445ec12adc1cf1a81e86
Eli <121198893+tired-tux@users.noreply.github.com>
2023-07-12 18:42:09 -0500

    Update code.rs

--- code.rs
+++ code.rs
@@ -1,16 +1,35 @@
+use std::fs::File;
+use std::io::{self, Write};
+
 fn main() {
-  println!("#!/bin/bash");
-  for q in 1..254 {
-    for w in 1..254 {
-      for e in 1..254 {
-        for r in 1..254 {
-          println!("ping -c 1 -i 0.2 {q}.{w}.{e}.{r}");
-          println!("if [ $? -eq 0 ]");
-          println!("then");
-          println!("echo -e {q}.{w}.{e}.{r}\n | tee -a ips");
-          println!("fi");
+    let mut ip_list = Vec::new();
+
+    for q in 1..=253 {
+        for w in 1..=253 {
+            for e in 1..=253 {
+                for r in 1..=253 {
+                    let ip = format!("{}.{}.{}.{}", q, w, e, r);
+
+                    let output = std::process::Command::new("ping")
+                        .args(&["-c", "1", "-i", "0.2", &ip])
+                        .output();
+
+                    match output {
+                        Ok(output) if output.status.success() => {
+                            ip_list.push(ip);
+                        }
+                        _ => {}
+                    }
+                }
+            }
         }
-      }
     }
-  }
+
+    let mut file = File::create("ip.list").expect("Failed to create ip.list file");
+
+    for ip in ip_list {
+        writeln!(file, "{}", ip).expect("Failed to write to ip.list file");
+    }
+
+    println!("Execution completed successfully.");
 }
